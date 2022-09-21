fe19a65844e4b35c33b564f468eb7ebf455b8056
MellowMarcus <114035155+MellowMarcus@users.noreply.github.com>
2022-09-21 19:22:35 -0400

    Update book.py

--- App/models/book.py
+++ App/models/book.py
@@ -1,14 +1,14 @@
 from App.database import db
 
 class Book(db.Model):
-    ISBN = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.Integer, primary_key=True)
     title =  db.Column(db.String, nullable=False)
     authorFname = db.Column(db.String, nullable=False)
     authorLname = db.Column(db.String, nullable=False)
     publiYear = db.Column(db.Date, nullable=False)
  
-def __init__(self, ISBN, title, authorFname, authorLname, publiYear):
-        self.ISBN = ISBN
+def __init__(self, isbn, title, authorFname, authorLname, publiYear):
+        self.isbn = isbn
         self.title = title
         self.authorFname = authorFname
         self.authorLname = authorLname
