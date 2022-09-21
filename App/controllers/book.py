4a7e0aa712e516b4d34a6bd25221a4907e28c351
MellowMarcus <114035155+MellowMarcus@users.noreply.github.com>
2022-09-21 19:20:30 -0400

    Update book.py

--- App/controllers/book.py
+++ App/controllers/book.py
@@ -1,20 +1,27 @@
 from App.models import Book
 from App.database import db
 
-def add_book(authorFname, authorLname, title, publiYear, ISBN):
-    newbook = Book(authorFname = authorFname, authorLname = authorLname, title = title, publiYear = publiYear, ISBN = ISBN):
+def add_book(isbn, authorFname, authorLname, title, publiYear):
+    newbook = Book(isbn = isbn, authorFname = authorFname, authorLname = authorLname, title = title, publiYear = publiYear):
     db.session.add(newbook)
     db.session.commit()
     return newbook
 
+def get_all_books():
+    return Book.query.all()
+
 def get_book_by_author(authorFname, authorLname):
     return Book.query.filter_by(authorFname=authorFname && authorLname = authorLname).first()
 
-def get_book(ISBN):
-    return Book.query.get(ISBN)
+def get_book_by_isbn(isbn):
+    return Book.query.filter_by(isbn= isbn).first()
 
-def get_all_books():
-    return Book.query.all()
+def get_book_by_Year(publiYear):
+    books = Book.query.all()
+    if not books:
+        return []
+    haul = [book.toJSON() for (publiYear && authorFname && authorLname) in books]
+    return haul
 
 def get_all_authors_json():
     books = Book.query.all()
